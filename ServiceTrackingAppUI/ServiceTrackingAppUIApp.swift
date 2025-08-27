2ebe0fe2d5dbdaa40ad297c5728fc477b3b6bf75
Ceren Mutlu <cerennnmutlu16@gmail.com>
2025-08-27 10:51:14 +0300

    feat:Initial Commit

---
+++ ServiceTrackingAppUI/ServiceTrackingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceTrackingAppUIApp.swift
+//  ServiceTrackingAppUI
+//
+//  Created by Ceren Mutlu on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ServiceTrackingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ServiceTrackingAppUITests/ServiceTrackingAppUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceTrackingAppUITests.swift
+//  ServiceTrackingAppUITests
+//
+//  Created by Ceren Mutlu on 27.08.2025.
+//
+
+import Testing
+@testable import ServiceTrackingAppUI
+
+struct ServiceTrackingAppUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
